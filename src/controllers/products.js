96cc0128fae42dc19fc1c0fc5e6b163c8fad7fa9
Le Trong Dat <datlt2306@gmail.com>
2022-03-12 07:23:19 +0700

    update

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -96,12 +96,94 @@ export const listRelated = async (req, res) => {
         .exec();
     res.json(related);
 }
-export const search = async (req, res) => {
-    const { query } = req.body;
-    if (query) {
-        const product = await Product.find({ $text: { $search: query } })
+const handleQuery = async (req, res, query) => {
+    const products = await Product.find({ $text: { $search: query } })
             .populate('category', '_id name')
+            .populate('subs', '_id name')
+            .populate('postedBy', '_id name')
             .exec()
         res.json(product)
+}
+const handlePrice = async (req, res, price) => {
+    try {
+        let products = Product.find({
+            price: {
+                gte: price[0],
+                lte: price[1]
+            }
+        })
+        .populate('category', '_id name')
+        .populate('subs', '_id name')
+        .populate('postedBy', '_id name')
+        .exec()
+        res.json(product)
+    } catch (error) {
+        
+    }
+}
+const handleCategory = async (req, res, category) => {
+    try {
+        let products = await Product.find({category})
+        .populate('category', '_id name')
+        .populate('subs', '_id name')
+        .populate('postedBy', '_id name')
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+    }
+}
+const handleShipping = async (req, res, shipping) => {
+    try {
+        let products = await Product.find({shipping})
+        .populate('category', '_id name')
+        .populate('subs', '_id name')
+        .populate('postedBy', '_id name')
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+    }
+}
+const handleColor = async (req, res, color) => {
+    try {
+        let products = await Product.find({color})
+        .populate('category', '_id name')
+        .populate('subs', '_id name')
+        .populate('postedBy', '_id name')
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+    }
+}
+const handleBrand = async (req, res, brand) => {
+    try {
+        let products = await Product.find({brand})
+        .populate('category', '_id name')
+        .populate('subs', '_id name')
+        .populate('postedBy', '_id name')
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const searchFilters = async (req, res) => {
+    const { query, price, category, shipping, color, brand } = req.body;
+    if(query){
+        await handleQuery(req, res, query);
+    }
+    // [20, 100]
+    if(price != undefined){
+        await handlePrice(req, res, price)
+    }
+    if(category){
+        handleCategory(req, res, category)
+    }
+    if(shipping){
+        handleShipping(req, res, shipping)
+    }
+    if(color){
+        handleColor(req, res, color)
+    }
+    if(brand){
+        handleBrand(req, res, brand)
     }
 }
